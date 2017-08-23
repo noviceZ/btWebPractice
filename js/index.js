f1ff4411aeeeac73f712ae5cfc050911f47f01e6
noviceZ <653312280@qq.com>
2017-08-23 13:55:17 +0800

    Signed-off-by: noviceZ <653312280@qq.com>

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+window.onload = function() {
+	/*
+	 * 设置标签之间距离，以适应屏幕宽度自动调整（巨幕部分巨幕底部图片文字模块.
+	 * 使.jumbotron-container-tag和.jumbotron-container-text两个<span>之间有足够间距）
+	 * */
+//	jumbotronConTag();            改用css媒体查询写
+}
+
+//function jumbotronConTag() {
+//	
+//}
+
+
